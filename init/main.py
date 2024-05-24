242ef704422b627639f3a9c8bd7943281f9636f0
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-05-24 23:14:49 +0900

    first

---
+++ init/main.py
@@ -0,0 +1,10 @@
+# https://www.python.org/downloads/
+# pathにチェックを入れてインストールする（Win）
+# 「Disable」をクリックして許可ボタンをおす（Win）
+# 同意をしてインストールする（Mac）
+# コマンドプロンプトでpython --versionと実行して確認する
+# VSCODEのプラグインで「Python」をインストールする
+# 再起動して.pyファイルを作成すると実行ボタンから実行できるようになる
+
+
+print("hello")
---
+++ print-basic/main.py
@@ -0,0 +1,13 @@
+# 上から下に向かって実行される
+# ただコメントアウトの行は実行されないためメモに使用される
+print("test")
+print("hello")
+print("hey")
+
+# データの種類は分けられているがターミナルでは文字として表示されることに注意
+# int型
+print(100)
+# float型
+print(1.2)
+#boolean型
+print(True)
