de933e2540de5f82dfdefab771fc509bedc272bc
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-05-29 21:21:34 +0900

    add:if-basic

---
+++ if-basic/main.py
@@ -0,0 +1,25 @@
+# if文は演算子でなくてもTrueとFalseを判定できる
+
+# 1をTrueにして0をFalseにできる
+# num = 1
+# num = 0
+# if(num):
+#   print("OK")
+# else:
+#   print("No")
+
+# 要素のあるリストをTrueに空のリストをFalseに判定できる
+# numbers = [1,2,3]
+# numbers = []
+# if(numbers):
+#   print("OK")
+# else:
+#   print("No")
+
+# 1文字以上の文字列をTrueに空文字をFalseに判定できる
+# str = "aaa"
+str = ""
+if(str):
+  print("OK")
+else:
+  print("No")
