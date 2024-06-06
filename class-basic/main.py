220f09c07ad765169e73e3632f9d83b6e758e918
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-06-07 04:53:30 +0900

    add:class-basic

---
+++ class-basic/main.py
@@ -0,0 +1,20 @@
+class Test:
+  # 変数や関数などいろんな種類のコードを保管できる
+  test_var = "hello"
+  test_list = [1,2,3]
+
+  # 関数には引数にselfが必要
+  def test_func(self):
+    print(1 + 2)
+    # selfはクラス自身のこと
+    print(self.test_var)
+
+# インスタンス化して初めて使える
+test = Test()
+print(test.test_var)
+print(test.test_list)
+test.test_func()
+
+# 関数と違って違う名前で複製することが可能
+# inst = Test()
+# print(inst.test_var)
