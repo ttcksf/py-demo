25a0854725e22ab30d982613ccd6bba42fb9dbd2
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-06-07 10:07:37 +0900

    add:module-import

---
+++ module-import/main.py
@@ -0,0 +1,14 @@
+import math
+import random
+import string
+from string import ascii_lowercase
+
+print(math.pi)
+print(math.sqrt(9))
+print(random.randint(0,10))
+# print(string.ascii_lowercase)
+# メソッドやプロパティをインポート文に入れるとライブラリ名を省略できる
+print(ascii_lowercase)
+
+# 標準モジュール
+# https://docs.python.org/3/library/index.html
