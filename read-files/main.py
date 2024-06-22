36f84404261d676fa99c656aa9a2d877f3eeaa79
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-06-23 01:03:19 +0900

    read-files

--- read-files/main.py
+++ read-files/main.py
@@ -6,7 +6,7 @@
 def filter_line(line):
   return "@" not in line
 
-with open("read-files/test2.txt") as f:
+with open("read-files/test1.txt") as f:
   lines = f.readline()
   # print(lines)
   # filterなど他の関数とセットにして読み込んだ内容を関数実行することもできる
