fc92d58d7673e8a2dd0b5618d1441099d2434dbd
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2024-07-05 01:00:40 +0900

    add:pillow-basic

---
+++ pillow-basic/main.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+image = Image.open("./image.png")
+print(image)
+# ファイル形式
+print(image.format)
+# ピクセル形式
+print(image.mode)
+# サイズ
+print(image.size)
+# サイズ変更（タプル型で入れる）
+new_image = image.resize((100,100))
+# 変更した画像を新しい画像として保存する
+new_image.save("./sample1.jpeg")
